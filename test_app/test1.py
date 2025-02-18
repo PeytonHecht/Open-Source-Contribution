8bb2a6e88b0fd2c9990d85a0d51d90b980abb1e1
PeytonHecht <62864282+PeytonHecht@users.noreply.github.com>
2025-02-17 20:13:14 -0500

    Inital app before bug fix
    The initial app for h2o wave that has the app code regarding the request of the good first issue

--- test_app/test1.py
+++ test_app/test1.py
@@ -2,9 +2,26 @@ from h2o_wave import main, app, Q, ui
 
 @app('/')
 async def serve(q: Q):
-    q.page['test'] = ui.markdown_card(
-        box='1 1 2 2',
-        title='Test App',
-        content='Hello, this is a test!'
-    )
+
+    table_rows = [
+        ui.table_row(name='row1', cells=['John', 'Doe', "7042 23rd Ave", "123-456-7890"]),
+        ui.table_row(name='row2', cells=['John', 'Doe', "7042 23rd Ave", "123-456-7890"]),
+        ui.table_row(name='row3', cells=['John', 'Doe', "7042 23rd Ave", "123-456-7890"]),
+    ]
+    column_names = ["name", "surname", "address", "phone"]
+    columns_to_test = {
+        "no_width": [ui.table_column(name=x, label=x) for x in column_names],
+        "min_width_0": [ui.table_column(name=x, label=x, min_width="0px") for x in column_names],
+        "max_width_100": [ui.table_column(name=x, label=x, max_width="200px") for x in column_names],
+        "min_width_0_and_max_width_100": [ui.table_column(name=x, label=x, min_width="0px", max_width="200px") for x in column_names]
+    }
+
+    q.page["meta"] = ui.meta_card("", layouts=[ui.layout("xs", width="800px", zones=[ui.zone("default")])])
+
+    for k in columns_to_test.keys():
+        q.page[k] = ui.form_card(box='default', items=[
+            ui.text_xl(k),
+            ui.table(name=k, columns=columns_to_test[k], rows=table_rows)
+        ])
+
     await q.page.save()
